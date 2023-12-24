fc561b0fd13765bbd225b6a5bde75c8dc711539b
Felipe Jesus <110308764+felipeollveira@users.noreply.github.com>
2023-12-24 16:08:12 -0300

    mongodb version 0

--- src/index.js
+++ src/index.js
@@ -1,31 +1,33 @@
 const express = require('express');
 const session = require('express-session')
 const server = express();
+const mongoose = require('mongoose');
 const cors = require('cors');
-const knex = require('./sql/connect')
+
 require('dotenv').config()
 const sessionSecret = process.env.private_key
 
 
 server.use(session({ secret: sessionSecret, resave: true, saveUninitialized: true }));
 
+mongoose.connect(process.env.MONGO_URI)
+const Post = require('./models/posts')
 
 server.use(cors());
 
-
-
 server.get('/', async (req, res) => {
-    try {
-      const posts = await knex('post').select('*');
-      return res.status(200).json({ posts });
-    } catch (error) {
-      console.error('Erro ao obter posts:', error.message);
-      return res.status(500).json({ error: 'Erro ao obter posts' });
-    }
-  });
+  try {
+    // Obter todos os posts usando Mongoose
+    const posts = await Post.find({});
 
+    return res.status(200).json({ posts });
+  } catch (error) {
+    console.error('Erro ao obter posts:', error.message);
+    return res.status(500).json({ error: 'Erro ao obter posts' });
+  }
+});
 // Iniciand o servidor
-const PORT = process.env.PORT || 3000; 
+const PORT = 5000; 
 server.listen(PORT, () => {
   console.log(`Servidor iniciado na porta ${PORT}`);
 });
--- src/sql/connect.js
+++ src/sql/connect.js
@@ -1,14 +1,15 @@
-const knex = require('knex')({
 
-    client: 'pg',
-    connection: {
-        user: 'lbrovmjk',
-        host: 'isabelle.db.elephantsql.com',
-        database: 'lbrovmjk',
-        password: 'T28jE2cC6AmzN6OFpBsc5X3DUYY95eAm',
-        
-    },
-    
-})
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-module.exports = knex
+
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log('MongoDb connected');
+}).catch((error) => {
+    console.log(error);
+});
+
+module.exports = {
+    mongoose: mongoose,
+};
