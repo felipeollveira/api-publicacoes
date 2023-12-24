11b020be9602be83f55fc29c9c94bdbc3f3e02b1
Felipe Jesus <110308764+felipeollveira@users.noreply.github.com>
2023-12-24 16:49:04 -0300

    mongodb version 1.9

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 require('dotenv').config();
 
 app.use(cors({
@@ -22,20 +22,36 @@ const connectDB = async () => {
   }
 };
 
-const Post = require('./models/posts');
-
-app.get('/', async (req, res) => {
-  try {
-    const posts = await Post.find({});
-    return res.json({ posts });
-  } catch (error) {
-    console.error('Error retrieving posts:', error.message);
-    return res.status(500).json({ error: 'Error retrieving posts' });
-  }
-});
-
-// Conectar-se ao banco de dados antes de escutar
+// Conectar-se ao MongoDB antes de iniciar o servidor
 connectDB().then(() => {
+  // Definindo o esquema e modelo após a conexão estar estabelecida
+  const postSchema = new mongoose.Schema({
+    titulo: String,
+    introducao: String,
+    desenvolvimento: String,
+    conclusao: String,
+    data: {
+      type: Date,
+      default: Date.now,
+    },
+    autor: String,
+    images: String,
+  });
+
+  const Post = mongoose.model('Post', postSchema);
+
+  // Rota para retornar os dados da coleção 'posts'
+  app.get('/', async (req, res) => {
+    try {
+      const posts = await Post.find({});
+      return res.json({ posts });
+    } catch (error) {
+      console.error('Error retrieving posts:', error.message);
+      return res.status(500).json({ error: 'Error retrieving posts' });
+    }
+  });
+
+  // Iniciar o servidor após a conexão ao MongoDB
   app.listen(PORT, () => {
     console.log(`Listening for requests on port ${PORT}`);
   });
