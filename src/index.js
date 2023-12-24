ccbf3ffeaae23f958aad634c104ef863cfd97d30
Felipe Jesus <110308764+felipeollveira@users.noreply.github.com>
2023-12-24 16:34:50 -0300

    port att

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors')
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3400;
 require('dotenv').config()
 
 app.use(cors())
@@ -43,11 +43,6 @@ app.get('/', async (req, res) => {
     console.error('Error retrieving posts:', error.message);
     return res.status(500).json({ error: 'Error retrieving posts' });
   }
-});
-
-// Rota padrão
-app.all('*', (req, res) => {
-  res.json({ "everything": "is awesome" });
 });
 
 // Conectar-se ao banco de dados antes de escutar
