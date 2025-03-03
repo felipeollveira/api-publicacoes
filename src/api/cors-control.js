52dca2f6c3f08232e24850a1725804c558a61e39
felipeollveira <masfelipe241@gmail.com>
2025-03-03 17:33:00 -0300

    cors system update: now i see

--- src/api/cors-control.js
+++ src/api/cors-control.js
@@ -1,18 +1,21 @@
 require('dotenv').config();
 
-// Middleware
+// Middleware 
 function logAccess(req, res, next) {
-  const origin = req.headers.origin || 'Localhost'; 
+  const origin = req.headers.origin || 'Localhost';
   const ip = req.headers['x-forwarded-for'] || req.ip; 
-
 
   const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
   if (!allowedOrigins.includes(origin)) {
+    // Loga tentativas de acesso bloqueadas
     console.error(`Tentativa de acesso bloqueada - Origem: ${origin}, IP: ${ip}`);
-    return res.status(403).json({ error: 'Acesso não permitido.' })
+    return res.status(403).json({ error: 'Acesso não permitido.', origin }); 
   }
 
-  next(); 
+  // Loga acessos permitidos
+  console.info(`Acesso permitido - Origem: ${origin}, IP: ${ip}`);
+
+  next();
 }
 
 // Configuração do CORS
@@ -20,15 +23,16 @@ const corsOptions = {
   origin: function (origin, callback) {
     const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
 
-    
+    // Permite requisições apenas de origens permitidas
     if (allowedOrigins.includes(origin)) {
-      callback(null, true); 
+      callback(null, true); // Permite a requisição
     } else {
-      callback(new Error('Acesso não permitido por CORS')); 
+      callback(new Error('Acesso não permitido por CORS')); // Rejeita requisições de origens não permitidas
     }
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
+// Exporta as funções
 module.exports = { logAccess, corsOptions };
